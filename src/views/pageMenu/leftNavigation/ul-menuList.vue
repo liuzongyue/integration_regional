a38435d9371848f433212e536be9b1500a26f4b0
liuzongyue <2559221072@qq.com>
2022-09-02 08:54:44 +0800

    目录结构

--- src/views/pageMenu/leftNavigation/ul-menuList.vue
+++ src/views/pageMenu/leftNavigation/ul-menuList.vue
@@ -1,23 +1,48 @@
 <template>
   <div>
-    <ul v-for="(item,index) in menuList" :key="item.label+index">
-      <div @click="item.show = !item.show">{{item.label}}</div>
-      <ul-menuList :menuList="item.children" v-if="item.children && item.show"></ul-menuList>
+    <ul v-for="(item, index) in menuList" :key="item.label + index">
+      <div @click="openPage($event, item)" class="menu-top">{{ item.label }}</div>
+      <ul-menuList :menuList="item.children" v-show="item.children && item.show" class="an-menu"></ul-menuList>
     </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "ulMenuList",
-    components: {
-    },
-    props: {
-      menuList: Array
-    },
-  }
+export default {
+  name: "ulMenuList",
+  components: {
+  },
+  props: {
+    menuList: Array
+  },
+  methods: {
+    openPage(e, item) {
+      item.show = !item.show
+      if (item.show) {
+        this.$nextTick(() => {
+          let node = e.target.parentNode.children[1]
+          if (node) {
+            node.style.height = item.children && (item.children.length * 90 + "px")
+            node.style.transition = "all 2s";
+          }
+        })
+      }
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
+.an-menu {
+  color: red;
+  background: blue;
+  overflow: hidden;
+}
 
+.menu-top {
+  height: 30px;
+  line-height: 30px;
+  display: flex;
+  justify-content: center;
+}
 </style>
