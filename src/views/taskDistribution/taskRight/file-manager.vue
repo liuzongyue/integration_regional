b0c1480dd4db51b7b4391409390fb9739c1f03e3
liuzongyue <2559221072@qq.com>
2022-09-20 17:36:49 +0800

    增加点击上传和拖拽上传文件功能

--- src/views/taskDistribution/taskRight/file-manager.vue
+++ src/views/taskDistribution/taskRight/file-manager.vue
@@ -4,10 +4,17 @@
     <div class="file-manager-main">
       <div class="form-box">
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-          <el-form-item label="任务级别" prop="region">
-            <el-select v-model="ruleForm.region" placeholder="请选择任务级别">
+          <el-form-item label="任务" prop="region">
+            <el-select v-model="ruleForm.region" placeholder="请选择任务">
               <el-option v-for="(item, index) in taskList" :key="item.value+index" :label="item.label" :value="item.value"></el-option>
             </el-select>
+          </el-form-item>
+          <el-form-item label="任务文件" prop="file">
+            <div>
+              <label for="inputFile" class="label_input" id="labelContanier">
+                <input type="file" id="inputFile" @change="selectFile">
+              </label>
+            </div>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="submitForm('ruleForm')">上传</el-button>
@@ -23,16 +30,17 @@
 export default {
   data() {
     return {
+      fileInput: null, //选择的文件
       ruleForm: {
-        name: '',
         region: '',
-        date1: '',
-        date2: '',
-        desc: ''
+        file: ''
       },
       rules: {
         region: [
           { required: true, message: '请选择活动区域', trigger: 'change' }
+        ],
+        file: [
+          { required: true, message: '请选择任务文件', trigger: 'change' }
         ],
       },
       taskList: [
@@ -58,6 +66,37 @@ export default {
   methods: {
     submitForm() {},
     resetForm() {},
+    selectFile(e) {
+      if(e.target.files.length > 0) {
+        this.ruleForm.file = e.target.files[0].name
+        this.fileInput = e.target.files[0]
+      }else{
+        this.ruleForm.file = ''
+        this.fileInput = null
+      }
+    },
+  },
+  mounted() {
+    let labelContanier = document.getElementById("labelContanier")
+    labelContanier.addEventListener("dragover", (e) => {
+      e.preventDefault()
+      if([...labelContanier.classList].indexOf("dragover") < 0) {
+        labelContanier.classList.add("dragover")
+      }
+    })
+    labelContanier.addEventListener("dragleave", (e) => {
+      if([...labelContanier.classList].indexOf("dragover") > 0) {
+        labelContanier.classList.remove("dragover")
+      }
+    })
+    labelContanier.addEventListener("drop", (e) => {
+      if([...labelContanier.classList].indexOf("dragover") > 0) {
+        labelContanier.classList.remove("dragover")
+        // 当文件拖拽到dropBox区域时,可以在该事件取到files
+	      const files = e.dataTransfer.files[0];
+        files && (this.fileInput = files)
+      }
+    })
   },
 }
 </script>
@@ -73,6 +112,17 @@ export default {
   .file-manager-main {
     padding: 20px 0;
     flex: 1;
+  }
+  .label_input{
+    display: inline-block;
+    width: 300px;
+    height: 150px;
+    background-color: #FFFFFF;
+    border-radius: 4px;
+    border: 1px solid #DCDFE6;
+  }
+  #inputFile{
+    display: none;
   }
 }
 </style>
