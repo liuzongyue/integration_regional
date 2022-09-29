3a4e566f3995a03a1f3e56558fa6a1dcf69a16c6
liuzongyue <2559221072@qq.com>
2022-09-29 17:18:13 +0800

    增加代码编辑器

--- src/views/pageMenu/code-runing/index.vue
+++ src/views/pageMenu/code-runing/index.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    页面1
+  <div class="code-runing">
+    <div class="edit-contanier-box">
+      <Editor v-model="code"></Editor>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import Editor from "vue2-ace-editor"
+export default {
+  components: {
+    Editor,
+  },
+  data() {
+    return {
+      code: null, //编写的代码
+    }
+  },
+  watch: {
+    code() {
+      console.log(this.code)
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-
+.code-runing {
+  height: 100%;
+  box-sizing: border-box;
+  padding-right: 40px;
+  .edit-contanier-box{
+    width: 100%;
+    height: 50%;
+    max-height: 300px;
+    border: 1px solid #dcdee2;
+    flex: 1;
+    flex-shrink: 1;
+  }
+}
+</style>
+<style lang="scss">
+  .code-runing{
+    .ace_print-margin{
+      display: none;
+    }
+  }
 </style>
