8701e583e0a126f79774529f4cd008bd5b0c68ef
liuzongyue <2559221072@qq.com>
2022-10-09 09:32:05 +0800

    完善代码编辑器

--- src/views/pageMenu/code-runing/index.vue
+++ src/views/pageMenu/code-runing/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="code-runing">
     <div class="edit-contanier-box">
-      <Editor v-model="code"></Editor>
+      <Editor v-model="code" :options="editorOptions"
+      @init="editorInit"
+      @input='codeChange'
+      lang="javascript"
+      theme="chrome"
+      ></Editor>
     </div>
   </div>
 </template>
@@ -15,7 +20,33 @@ export default {
   data() {
     return {
       code: null, //编写的代码
+      editorOptions: {
+        // 设置代码编辑器的样式
+        enableBasicAutocompletion: true, //启用基本自动完成
+        enableSnippets: true, // 启用代码段
+        enableLiveAutocompletion: true, //启用实时自动完成
+        tabSize: 2, //标签大小
+        fontSize: 18, //设置字号
+        showPrintMargin: false, //去除编辑器里的竖线
+      }
     }
+  },
+  methods: {
+    codeChange(val) {
+      // console.log(val);
+    },
+    editorInit() {
+      require('brace/theme/chrome');
+      require('brace/ext/language_tools'); //language extension prerequsite...
+      require('brace/mode/yaml');
+      require('brace/mode/json');
+      require('brace/mode/less');
+      require('brace/snippets/json');
+      require('brace/mode/lua');
+      require('brace/snippets/lua');
+      require('brace/mode/javascript');
+      require('brace/snippets/javascript');
+    },
   },
   watch: {
     code() {
@@ -30,7 +61,8 @@ export default {
   height: 100%;
   box-sizing: border-box;
   padding-right: 40px;
-  .edit-contanier-box{
+
+  .edit-contanier-box {
     width: 100%;
     height: 50%;
     max-height: 300px;
@@ -41,9 +73,9 @@ export default {
 }
 </style>
 <style lang="scss">
-  .code-runing{
-    .ace_print-margin{
-      display: none;
-    }
+.code-runing {
+  .ace_print-margin {
+    // display: none;
   }
+}
 </style>
